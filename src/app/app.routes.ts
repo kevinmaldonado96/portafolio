1067d78e3115b7e3ffc5cb51e1136ee53e4e6bfe
kevinmaldonado96 <kevin_maldonado19@outlook.com>
2024-12-18 22:57:45 -0500

    seccion about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'/about', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { enableTracing: false, scrollPositionRestoration: 'enabled' })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
