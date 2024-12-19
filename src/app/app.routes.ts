95c2e153bba9690ff03149fc7260e625a798d74f
kevinmaldonado96 <kevin_maldonado19@outlook.com>
2024-12-19 18:47:00 -0500

    añadir iconos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
+import { ResumeComponent } from './resume/resume.component';
 
 export const routes: Routes = [
     { path: '', redirectTo:'/about', pathMatch: 'full' },
     { path: 'about', component: AboutComponent },
+    { path: 'resume', component: ResumeComponent }
 ];
 
 @NgModule({
