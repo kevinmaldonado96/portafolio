29b56f843ce98cc6c9da2d2defd0569358ee1f44
kevinmaldonado96 <kevin_maldonado19@outlook.com>
2025-01-01 19:56:06 -0500

    changes experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { ResumeComponent } from './resume/resume.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
 
 export const routes: Routes = [
     { path: '', redirectTo:'/about', pathMatch: 'full' },
     { path: 'about', component: AboutComponent },
-    { path: 'resume', component: ResumeComponent }
+    { path: 'education', component: ResumeComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'skills', component: SkillsComponent }
 ];
 
 @NgModule({
